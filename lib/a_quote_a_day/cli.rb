0707f74cb1e999e675d254bdfa037ea1bfeef099
juan siu <juan.siu@gmail.com>
2019-08-08 23:26:50 +0000

    reduce method categorie using a class variable

--- lib/a_quote_a_day/cli.rb
+++ lib/a_quote_a_day/cli.rb
@@ -1,6 +1,7 @@
 class AQuoteADay::CLI
  @@favorite = []
-  
+@@types = ["peace", "life", "short", "happiness", "gratitude", "friendship", "kindness", "words-of-wisdom"]
+
   def call
     new = Scraper.new.make_quote
     new.call
@@ -9,7 +10,7 @@ class AQuoteADay::CLI
 
   def menu
     puts "**Welcome to A Quote A Day!**"
-    puts "" 
+    puts ""
     puts "Here are your options, please type the number of what do you want to access."
     puts ""
     puts <<-DOC
@@ -17,91 +18,55 @@ class AQuoteADay::CLI
 2. Favorites
 3. Exit
     DOC
-    
+
     input = gets.strip.downcase
     if input == "1"
-      categories 
+      categories
     elsif input == "2"
-      favorites 
+      favorites
     elsif input == "3"
     puts "Enjoy the rest of your day!."
-      exit 
+      exit
     else
       puts "Invalid answer!."
       menu
     end
   end
-  
+
   def categories
     puts "Here is the list of categories, choose the number of the option you want."
     puts ""
     puts <<-DOC
-1. Motivational
-2. Life 
+1. Peace
+2. Life
 3. Short
-4. Wisdom
-5. Happiness 
-6. Gratitude
-7. Frienship 
-8. Kindness
+4. Happiness
+5. Gratitude
+6. Frienship
+7. Kindness
+8. Wisdom
 9. Back to Main Menu
 DOC
 
-     input = gets.strip.downcase
-    if input == "1"
-      new = Scraper.new("http://wisdomquotes.com/motivational-quotes/").make_quote
-      new.call
-      new.categorie = "motivational"
-      puts " "
-      categories
-    elsif input == "2"
-      new = Scraper.new("http://wisdomquotes.com/life-quotes/").make_quote
-      new.call
-      new.categorie = "life"
-      puts " "
-      categories
-    elsif input == "3"
-      new = Scraper.new("http://wisdomquotes.com/short-quotes/").make_quote
-      new.call
-      new.categorie = "short"
-      puts " "
-      categories
-    elsif input == "4"
-      new = Scraper.new("http://wisdomquotes.com/words-of-wisdom/").make_quote
-      new.call
-      new.categorie = "wisdom"
-      puts " "
-      categories
-    elsif input == "5"
-      new = Scraper.new("http://wisdomquotes.com/happiness-quotes/").make_quote
-      new.call
-      new.categorie = "happiness"
-      puts " "
-      categories
-    elsif input == "6"
-      new = Scraper.new("http://wisdomquotes.com/gratitude-quotes/").make_quote
-      new.call
-      new.categorie = "gratitude"
-      puts " "
-      categories
-    elsif input == "7"
-      new = Scraper.new("http://wisdomquotes.com/friendship-quotes/").make_quote
-      new.call
-      new.categorie = "friendship"
-      puts " "
-      categories
-    elsif input == "8"
-      new = Scraper.new("http://wisdomquotes.com/kindness-quotes/").make_quote
-      new.call
-      new.categorie = "kindness"
-      puts " "
-      categories
-    elsif input == "9"
-      menu 
-    else
-      puts "Invalid answer!"
-      categories
-    end
+     input = gets.to_i - 1
+      if input.between?(0,6)
+          new_selection = Scraper.new("http://wisdomquotes.com/#{@@types[input]}-quotes/").make_quote
+          new_selection.call
+          new_selection.categorie = "#{@@types[input]}"
+          puts " "
+          categories
+      elsif input == 7
+        new_selection = Scraper.new("http://wisdomquotes.com/#{@@types[input]}/").make_quote
+        new_selection.call
+        new_selection.categorie = "#{@@types[input]}"
+        puts " "
+        categories
+      elsif input == 8
+          menu
+      else
+          puts "Invalid answer!"
+          categories
+      end
   end
 
   def favorites
@@ -120,7 +85,7 @@ DOC
       if input == "0"
         puts Quote.all
       elsif input == "1"
-        puts Quote.favorite 
+        puts Quote.favorite
         favorites
       elsif input == "2"
         puts Quote.call_last
@@ -145,8 +110,8 @@ DOC
         Quote.find_by_categorie(categorie)
         favorites
       elsif input == "7"
-        menu   
-      else 
+        menu
+      else
         puts "Invalid answer!."
         favorites
       end
